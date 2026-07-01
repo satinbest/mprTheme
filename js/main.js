de0480b2d8e71aa488b3cb50a19e19dd570dccf3
Hossein <2ksky92@gmail.com>
2026-07-02 01:05:35 +0330

    create => hero-prodct section

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(".owl-carousel").owlCarousel({
+$(".m-slider").owlCarousel({
   loop: true,
   margin: 0,
   nav: true,
@@ -17,6 +17,29 @@ $(".owl-carousel").owlCarousel({
     },
     1000: {
       items: 1,
+    },
+  },
+});
+$(".product-slider").owlCarousel({
+  loop: true,
+  margin: 0,
+  nav: false,
+  dots: false,
+  rtl: true,
+  autoplay: true,
+  navText: [
+    "<i class='fa-solid fa-chevron-right'></i>",
+    "<i class='fa-solid fa-chevron-left'></i>",
+  ],
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 5,
+    },
+    1000: {
+      items: 5,
     },
   },
 });
