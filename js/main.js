e502d0e3eafb52d4e7105402d33728bb824a2383
Hossein <2ksky92@gmail.com>
2026-07-03 02:09:53 +0330

    create => amazing product section part 1

--- js/main.js
+++ js/main.js
@@ -27,10 +27,27 @@ $(".product-slider").owlCarousel({
   dots: false,
   rtl: true,
   autoplay: true,
-  navText: [
-    "<i class='fa-solid fa-chevron-right'></i>",
-    "<i class='fa-solid fa-chevron-left'></i>",
-  ],
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 5,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
+
+$(".amazing-slider").owlCarousel({
+  loop: false,
+  margin: 0,
+  mouseDrag:false,
+  nav: false,
+  dots: false,
+  rtl: true,
+  autoplay: false,
   responsive: {
     0: {
       items: 1,
