fad6a73a925eccd0dc37a115df19b67b481f2252
Hossein <2ksky92@gmail.com>
2026-07-04 02:17:16 +0330

    create => amazing product section part 2 - add timer

--- js/main.js
+++ js/main.js
@@ -60,3 +60,57 @@ $(".amazing-slider").owlCarousel({
     },
   },
 });
+
+/***********timer Amazing***********/
+jQuery(document).ready(function (){
+    function countdownTimer() {
+        var countdownElements = document.querySelectorAll('.box-timer');
+        countdownElements.forEach(function (countdownElement) {
+            var daysElements = countdownElement.querySelectorAll('.days-value');
+            var hoursElements = countdownElement.querySelectorAll('.hours-value');
+            var minutesElements = countdownElement.querySelectorAll('.minutes-value');
+            var secondsElements = countdownElement.querySelectorAll('.seconds-value');
+            var messageElement = countdownElement.querySelector('.timer-message');
+            var messagesElement = countdownElement.querySelector('.massages-heddin');
+            var targetYear = parseInt(countdownElement.dataset.targetYear);
+            var targetMonth = parseInt(countdownElement.dataset.targetMonth) - 1;
+            var targetDay = parseInt(countdownElement.dataset.targetDay);
+            var targetHour = parseInt(countdownElement.dataset.targetHour);
+            var targetMinute = parseInt(countdownElement.dataset.targetMinute);
+            var targetSecond = parseInt(countdownElement.dataset.targetSecond);
+            var targetDate = new Date(targetYear, targetMonth, targetDay, targetHour, targetMinute, targetSecond);
+
+            function updateTimer() {
+                var now = new Date().getTime();
+                var timeRemaining = targetDate - now;
+                var days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+                var hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                var minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+                var seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+                for (var i = 0; i < daysElements.length; i++) {
+                    daysElements[i].innerHTML = days;
+                }
+                for (var i = 0; i < hoursElements.length; i++) {
+                    hoursElements[i].innerHTML = hours;
+                }
+                for (var i = 0; i < minutesElements.length; i++) {
+                    minutesElements[i].innerHTML = minutes;
+                }
+                for (var i = 0; i < secondsElements.length; i++) {
+                    secondsElements[i].innerHTML = seconds;
+                }
+                if (timeRemaining > 0) {
+                    setTimeout(updateTimer, 1000);
+                } else {
+                    messagesElement.style.display = 'none';
+                    /*messageElement.style.display = 'block';
+                    var messageTextElement = messageElement.querySelector('.timer-message-text');
+                    messageTextElement.innerHTML = 'تایمر به پایان رسیده است';*/
+                }
+            }
+
+            updateTimer();
+        });
+    }
+    countdownTimer();
+});
