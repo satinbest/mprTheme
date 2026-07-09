7c75b7529d8520b9c68df76c4afa79dbba8ff9b7
Hossein mohammadpour <2ksky92@gmail.com>
2026-07-10 02:20:20 +0330

    add: login modal

--- js/main.js
+++ js/main.js
@@ -43,7 +43,7 @@ $(".product-slider").owlCarousel({
 $(".amazing-slider").owlCarousel({
   loop: false,
   margin: 0,
-  mouseDrag:false,
+  mouseDrag: false,
   nav: false,
   dots: false,
   rtl: true,
@@ -63,10 +63,10 @@ $(".amazing-slider").owlCarousel({
 $(".special-slider").owlCarousel({
   loop: false,
   margin: 0,
-  dots:true,
+  dots: true,
   nav: false,
   rtl: true,
-  dotsData:true,
+  dotsData: true,
   autoplay: false,
   responsive: {
     0: {
@@ -83,7 +83,7 @@ $(".special-slider").owlCarousel({
 $(".insta-slider").owlCarousel({
   loop: true,
   margin: 0,
-  dots:false,
+  dots: false,
   nav: false,
   rtl: true,
   autoplay: false,
@@ -102,11 +102,11 @@ $(".insta-slider").owlCarousel({
 $(".brand-slider").owlCarousel({
   loop: true,
   margin: 0,
-  dots:false,
+  dots: false,
   nav: true,
   rtl: true,
   autoplay: false,
-    navText: [
+  navText: [
     "<i class='fa-solid fa-chevron-right'></i>",
     "<i class='fa-solid fa-chevron-left'></i>",
   ],
@@ -123,55 +123,94 @@ $(".brand-slider").owlCarousel({
   },
 });
 /***********timer Amazing***********/
-jQuery(document).ready(function (){
-    function countdownTimer() {
-        var countdownElements = document.querySelectorAll('.box-timer');
-        countdownElements.forEach(function (countdownElement) {
-            var daysElements = countdownElement.querySelectorAll('.days-value');
-            var hoursElements = countdownElement.querySelectorAll('.hours-value');
-            var minutesElements = countdownElement.querySelectorAll('.minutes-value');
-            var secondsElements = countdownElement.querySelectorAll('.seconds-value');
-            var messageElement = countdownElement.querySelector('.timer-message');
-            var messagesElement = countdownElement.querySelector('.massages-heddin');
-            var targetYear = parseInt(countdownElement.dataset.targetYear);
-            var targetMonth = parseInt(countdownElement.dataset.targetMonth) - 1;
-            var targetDay = parseInt(countdownElement.dataset.targetDay);
-            var targetHour = parseInt(countdownElement.dataset.targetHour);
-            var targetMinute = parseInt(countdownElement.dataset.targetMinute);
-            var targetSecond = parseInt(countdownElement.dataset.targetSecond);
-            var targetDate = new Date(targetYear, targetMonth, targetDay, targetHour, targetMinute, targetSecond);
+$(document).ready(function () {
+  function countdownTimer() {
+    var countdownElements = document.querySelectorAll(".box-timer");
+    countdownElements.forEach(function (countdownElement) {
+      var daysElements = countdownElement.querySelectorAll(".days-value");
+      var hoursElements = countdownElement.querySelectorAll(".hours-value");
+      var minutesElements = countdownElement.querySelectorAll(".minutes-value");
+      var secondsElements = countdownElement.querySelectorAll(".seconds-value");
+      var messageElement = countdownElement.querySelector(".timer-message");
+      var messagesElement = countdownElement.querySelector(".massages-heddin");
+      var targetYear = parseInt(countdownElement.dataset.targetYear);
+      var targetMonth = parseInt(countdownElement.dataset.targetMonth) - 1;
+      var targetDay = parseInt(countdownElement.dataset.targetDay);
+      var targetHour = parseInt(countdownElement.dataset.targetHour);
+      var targetMinute = parseInt(countdownElement.dataset.targetMinute);
+      var targetSecond = parseInt(countdownElement.dataset.targetSecond);
+      var targetDate = new Date(
+        targetYear,
+        targetMonth,
+        targetDay,
+        targetHour,
+        targetMinute,
+        targetSecond,
+      );
 
-            function updateTimer() {
-                var now = new Date().getTime();
-                var timeRemaining = targetDate - now;
-                var days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
-                var hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-                var minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-                var seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
-                for (var i = 0; i < daysElements.length; i++) {
-                    daysElements[i].innerHTML = days;
-                }
-                for (var i = 0; i < hoursElements.length; i++) {
-                    hoursElements[i].innerHTML = hours;
-                }
-                for (var i = 0; i < minutesElements.length; i++) {
-                    minutesElements[i].innerHTML = minutes;
-                }
-                for (var i = 0; i < secondsElements.length; i++) {
-                    secondsElements[i].innerHTML = seconds;
-                }
-                if (timeRemaining > 0) {
-                    setTimeout(updateTimer, 1000);
-                } else {
-                    messagesElement.style.display = 'none';
-                    /*messageElement.style.display = 'block';
+      function updateTimer() {
+        var now = new Date().getTime();
+        var timeRemaining = targetDate - now;
+        var days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+        var hours = Math.floor(
+          (timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+        );
+        var minutes = Math.floor(
+          (timeRemaining % (1000 * 60 * 60)) / (1000 * 60),
+        );
+        var seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+        for (var i = 0; i < daysElements.length; i++) {
+          daysElements[i].innerHTML = days;
+        }
+        for (var i = 0; i < hoursElements.length; i++) {
+          hoursElements[i].innerHTML = hours;
+        }
+        for (var i = 0; i < minutesElements.length; i++) {
+          minutesElements[i].innerHTML = minutes;
+        }
+        for (var i = 0; i < secondsElements.length; i++) {
+          secondsElements[i].innerHTML = seconds;
+        }
+        if (timeRemaining > 0) {
+          setTimeout(updateTimer, 1000);
+        } else {
+          messagesElement.style.display = "none";
+          /*messageElement.style.display = 'block';
                     var messageTextElement = messageElement.querySelector('.timer-message-text');
                     messageTextElement.innerHTML = 'تایمر به پایان رسیده است';*/
-                }
-            }
+        }
+      }
 
-            updateTimer();
-        });
+      updateTimer();
+    });
+  }
+  countdownTimer();
+});
+
+// start modal-login
+$(document).ready(function (){
+    var modal_login = document.getElementById("modal_login");
+// Get the button that opens the modal
+    var btn_modal_login = document.getElementById("btn_modal_login");
+// Get the <span> element that closes the modal
+    var close = document.getElementsByClassName("close")[0];
+// When the user clicks the button, open the modal
+    btn_modal_login.onclick = function() {
+        modal_login.style.display = "block";
     }
-    countdownTimer();
+// When the user clicks on <span> (x), close the modal
+    close.onclick = function() {
+        modal_login.style.display = "none";
+    }
+// When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal_login) {
+            modal_login.style.display = "none";
+        }
+    }
+})
+
+// end modal login
+$('.message a').click(function(){
+    $('.form-login-mprtheme form').animate({height: "toggle", opacity: "toggle"}, "slow");
 });
